d09983d28ec38e844da8158a5d71c4360a357442
Joel Stockamp <stockampj@gmail.com>
2020-08-30 00:35:13 -0700

    change app to functional component

--- src/components/App.js
+++ src/components/App.js
@@ -3,18 +3,13 @@ import React, { Component } from 'react'
 // import {ipcRenderer} from 'electron'
 import TestButton from './TestButton.jsx'
 
-class App extends Component {
-  constructor(props){
-    super(props)
-  }
+function App() {
 
-  render() {
     return (
       <div>
         <TestButton/>
       </div>
     )
   }
-}
 
 export default App
