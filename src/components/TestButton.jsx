a516b8b4661b00e91643cac307d1208b757a2e20
Joel Stockamp <stockampj@gmail.com>
2020-08-30 01:00:23 -0700

    convert test button to functional component and use effects instead of other life cycle methods

--- src/components/TestButton.jsx
+++ src/components/TestButton.jsx
@@ -1,25 +1,19 @@
 import '../assets/css/App.css'
-import React, { Component } from 'react'
+import React, { Component, useEffect } from 'react'
 import {ipcRenderer} from 'electron'
 
-export default class TestButton extends Component {
-  constructor(props){
-    super(props)
+export default function TestButton() {
 
-    this.handleButtonClick = this.handleButtonClick.bind(this)
-    this.handleMessageFromMain = this.handleMessageFromMain.bind(this)
-    this.handleItemsList = this.handleItemsList.bind(this)
-  }
-  componentDidMount(){
-    ipcRenderer.on('button-click-received', this.handleMessageFromMain)
-    ipcRenderer.on('items-list', this.handleItemsList)
-  }
-  componentWillUnmount() {
-    ipcRenderer.removeListener('button-click-received')
-    ipcRenderer.removeListener('items-list')
-  }
+  useEffect(()=>{
+    ipcRenderer.on('button-click-received', handleMessageFromMain)
+    ipcRenderer.on('items-list', handleItemsList)
+    return ()=>{
+      ipcRenderer.removeListener('button-click-received', handleMessageFromMain)
+      ipcRenderer.removeListener('items-list', handleItemsList)
+    }
+  })
 
-  handleButtonClick(){
+  function handleButtonClick(){
     console.log('button click')
     const testStoreItem = {
       name: 'Bob',
@@ -28,19 +22,17 @@ export default class TestButton extends Component {
     ipcRenderer.send('add-item', JSON.stringify(testStoreItem))
   }
 
-  handleMessageFromMain(event, arg){
+  function handleMessageFromMain(event, arg){
     const unpackedArg = JSON.parse(arg)
     console.log('handle message from main', unpackedArg)
   }
 
-  handleItemsList(event, arg){
+  function handleItemsList(event, arg){
     const unpackedArg = JSON.parse(arg)
     console.log('handle message from main', unpackedArg)
   }
 
-  render() {
-    return (
-        <button className="test-button" onClick={()=> this.handleButtonClick()}>click me</button>
-    )
-  }
+  return (
+      <button className="test-button" onClick={()=> handleButtonClick()}>click me</button>
+  )
 }
