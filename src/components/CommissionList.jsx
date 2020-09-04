e13ade361c4a05d520b622f5668f17a546930774
Joel Stockamp <stockampj@gmail.com>
2020-09-04 13:47:13 -0700

    add pngs

--- src/components/CommissionList.jsx
+++ src/components/CommissionList.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 
+
 export default function CommissionList (){
   return (
     <div>Commission List</div>
--- src/components/TopPanel.jsx
+++ src/components/TopPanel.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import './TopPanel.css';
 
+
 export default function TopPanel() {
   return (
     <div className="top-panel-wrapper">
       <div className="top-panel">
         <h1 className="app-name">Commission Tracker</h1>
+        {/* <img  alt=""/> */}
       </div>
       <div className="top-panel-spacer"></div>
     </div>
