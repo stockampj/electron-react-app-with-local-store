dc01c03a967f701e6490829ea408b7d7e2c9eff4
Joel Stockamp <stockampj@gmail.com>
2020-09-08 09:26:45 -0700

    add new fonts and adjusted css to accomodate new fonts. Also changed z-index of hover layer so that it isn't hidden behind something rendered later

--- src/components/TopPanel.jsx
+++ src/components/TopPanel.jsx
@@ -6,8 +6,7 @@ export default function TopPanel() {
   return (
     <div className="top-panel-wrapper">
       <div className="top-panel">
-        <h1 className="app-name">Commission Tracker</h1>
-        {/* <img  alt=""/> */}
+        <h1 className="app-name">Commission Tracker <i class="fas fa-calculator"></i></h1>
       </div>
       <div className="top-panel-spacer"></div>
     </div>
